25c9e3cb8585e2642322f9527ab540462ad0c4d1
SorushAini <sorush.aini@futuregames.nu>
2021-09-14 19:47:50 +0200

    updates icecleaner

---
+++ FG_TM2_AS1_2DShooter/Assets/Scripts/cs_IceCleaner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cs_IceCleaner : MonoBehaviour
+{
+    public List<Transform> corners = new List<Transform>();
+
+    public int TargetCorner;
+    public int FromCorner;
+    public float Speed;
+  
+    public void Awake()
+    {
+        TargetCorner = Random.Range(0, corners.Count);
+        transform.position = corners[TargetCorner].position;
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, corners[TargetCorner].position) < 0.1f)
+        {
+            RandomizeCornerTarget();
+        }
+
+        transform.position = transform.position + (corners[TargetCorner].position - transform.position).normalized * Speed * Time.deltaTime;
+    }
+
+    public void RandomizeCornerTarget()
+    {
+        int OldTarget = TargetCorner;
+
+        TargetCorner = Random.Range(0, corners.Count);
+        while (TargetCorner == FromCorner || TargetCorner == OldTarget)
+        {
+            TargetCorner = Random.Range(0, corners.Count);
+        }
+
+        FromCorner = OldTarget;
+    }
+}

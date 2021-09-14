8dfc5e354fca2392edc589e4e513a9388bfa93e2
Sorush <sorush.aini@futuregames.nu>
2021-09-14 13:34:36 +0200

    update icecleaner

--- FG_TM2_AS1_2DShooter/Assets/Scripts/cs_RandomMovement.cs
+++ FG_TM2_AS1_2DShooter/Assets/Scripts/cs_RandomMovement.cs
@@ -7,42 +7,38 @@ public class cs_RandomMovement : MonoBehaviour
 {
 
     public float timer;
-
     public float Timelimit;
-
     public float speed;
-
     public Vector3 target;
-    
 
-    void Start()
-    {
-        
-    }
 
-    
+
+
     void Update()
     {
         timer += Time.deltaTime;
 
-        if(timer >= Timelimit)
+        if (timer >= Timelimit)
         {
             newTarget();
             timer = 0;
-
         }
-       
-        transform.position += speed * (target - transform.position) * Time.deltaTime;
-       // transform.Translate(Vector3.up * Time.deltaTime);
+
+        // transform.position += (target - transform.position).normalized * speed * Time.deltaTime;
+
+        transform.Translate(Vector3.up * Time.deltaTime);
+
+
+
     }
-    void newTarget ()
+    void newTarget()
     {
         float myX = gameObject.transform.position.x;
         float myY = gameObject.transform.position.y;
 
-        float xPos = myX + Random.Range(-5, 5);
-        float yPos = myY + Random.Range(-5, 5);
-        target = new Vector2(xPos,yPos);
+        float xPos = myX + Random.Range(-15, 15);
+        float yPos = myY + Random.Range(-15, 15);
+        target = new Vector2(xPos, yPos);
     }
 
     public IEnumerator IceCleanerMovement()
@@ -57,5 +53,5 @@ public class cs_RandomMovement : MonoBehaviour
 
 
     }
-
 }
+

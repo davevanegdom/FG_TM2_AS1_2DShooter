1caafb3454b8282d7d15e9350ab4f10fff2cd72b
Sorush <sorush.aini@futuregames.nu>
2021-09-16 11:04:04 +0200

    camera updates

--- FG_TM2_AS1_2DShooter/Assets/Scripts/cs_CameraFollow.cs
+++ FG_TM2_AS1_2DShooter/Assets/Scripts/cs_CameraFollow.cs
@@ -1,18 +1,20 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class cs_CameraFollow : MonoBehaviour
 {
 
     [SerializeField] private Transform target;
-
     [SerializeField] [Range(0.01f, 10f)]
-
     private float smoothSpeed = 4f;
-    
     [SerializeField] private Vector3 offset;
-
-    
     private Vector3 velocity = Vector3.zero;
+
+    public float MinDist = 2;
+    public float MaxDist = 5;
+    public float MaxVelocity = 2;
+    
+    public Rigidbody2D playerBody;
 
 
     private void Update()
@@ -22,14 +24,15 @@ public class cs_CameraFollow : MonoBehaviour
 
     private void FixedUpdate()
     {
-        transform.position = Damp_Vector3(transform.position, new Vector3(target.transform.position.x, target.transform.position.y, -10), smoothSpeed, Time.deltaTime);
+        float TargetDist = MinDist + (MaxDist - MinDist) * Mathf.Min(1.0f, playerBody.velocity.magnitude / MaxVelocity);
+        float cSize = TargetDist;
+        Camera.main.orthographicSize = cSize;
+
+        transform.position = Damp_Vector3(transform.position, new Vector3(target.transform.position.x, target.transform.position.y, - 10), smoothSpeed, Time.deltaTime);
+        
     }
 
-    public static float Damp(float a, float b, float lambda, float dt)
-    {
-        return Mathf.Lerp(a, b, 1 - Mathf.Exp(-lambda * dt));
-    }
-
+    
     public static Vector3 Damp_Vector3(Vector3 a, Vector3 b, float lambda, float dt)
     {
         return Vector3.Lerp(a, b, 1 - Mathf.Exp(-lambda * dt));
